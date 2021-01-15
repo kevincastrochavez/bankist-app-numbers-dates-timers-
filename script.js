813b261f023953de178b45a5f5ca6d0fac68c446
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2021-01-15 12:58:34 -0600

    learned how to use the remainder operator

--- script.js
+++ script.js
@@ -281,45 +281,68 @@ btnSort.addEventListener('click', function (e) {
 // console.log(Number.isInteger('23'));
 // console.log(Number.isInteger(23.0));
 
+// ///////////////////////////////////////
+// // Math and Rounding
+
+// console.log(Math.sqrt(25));
+// console.log(28 ** (1 / 2));
+// console.log(8 ** (1 / 3));
+
+// console.log(Math.max(5, 18, 23, 11, 2));
+// console.log(Math.max(5, 18, '23', 11, 2));
+// console.log(Math.max(5, 18, '23px', 11, 2));
+
+// console.log(Math.min(5, 18, '23', 11, 2));
+
+// console.log(Math.PI * Number.parseFloat('10px') ** 2);
+
+// console.log(Math.trunc(Math.random() * 6) + 1);
+
+// // Get random number between two values
+// const randomInt = (min, max) =>
+//   Math.floor(Math.random() * (max - min) + 1) + min;
+// console.log(randomInt(10, 20));
+
+// // Round integers
+// console.log(Math.trunc(23.3));
+
+// console.log(Math.round(23.3));
+// console.log(Math.round(23.9));
+
+// console.log(Math.ceil(23.3));
+// console.log(Math.ceil(23.9));
+
+// console.log(Math.floor(23.3));
+// console.log(Math.floor('23.9'));
+
+// console.log(Math.trunc(-23.9));
+// console.log(Math.floor(-23.9));
+
+// // Rounding decimals
+// console.log((2.7).toFixed(0));
+// console.log((2.7).toFixed(3));
+// console.log((2.789).toFixed(2));
+// console.log(+(2.789).toFixed(2));
+
 ///////////////////////////////////////
-// Math and Rounding
+// The Remainder Operator
 
-console.log(Math.sqrt(25));
-console.log(28 ** (1 / 2));
-console.log(8 ** (1 / 3));
+console.log(5 % 2); // <= THIS
+console.log(5 / 2);
 
-console.log(Math.max(5, 18, 23, 11, 2));
-console.log(Math.max(5, 18, '23', 11, 2));
-console.log(Math.max(5, 18, '23px', 11, 2));
+const isEven = n => n % 2 === 0;
+console.log(isEven(8));
+console.log(isEven(28));
+console.log(isEven(123));
 
-console.log(Math.min(5, 18, '23', 11, 2));
-
-console.log(Math.PI * Number.parseFloat('10px') ** 2);
-
-console.log(Math.trunc(Math.random() * 6) + 1);
-
-// Get random number between two values
-const randomInt = (min, max) =>
-  Math.floor(Math.random() * (max - min) + 1) + min;
-console.log(randomInt(10, 20));
-
-// Round integers
-console.log(Math.trunc(23.3));
-
-console.log(Math.round(23.3));
-console.log(Math.round(23.9));
-
-console.log(Math.ceil(23.3));
-console.log(Math.ceil(23.9));
-
-console.log(Math.floor(23.3));
-console.log(Math.floor('23.9'));
-
-console.log(Math.trunc(-23.9));
-console.log(Math.floor(-23.9));
-
-// Rounding decimals
-console.log((2.7).toFixed(0));
-console.log((2.7).toFixed(3));
-console.log((2.789).toFixed(2));
-console.log(+(2.789).toFixed(2));
+labelBalance.addEventListener('click', function () {
+  const thisArray = [...document.querySelectorAll('.movements__row')];
+  thisArray.forEach(function (row, i) {
+    if (i % 2 === 0) {
+      row.style.backgroundColor = 'orange';
+    }
+    if (i % 3 === 0) {
+      row.style.backgroundColor = 'blue';
+    }
+  });
+});
